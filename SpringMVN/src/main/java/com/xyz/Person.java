5e3682369d8fb625c90a617217f62bf600689f9e
rajeev <kumar.rajeevranjan10@yahoo.com>
2023-10-29 18:00:52 -0400

    @Autowire and @Qualifier Concepts

---
+++ SpringMVN/src/main/java/com/xyz/Person.java
@@ -0,0 +1,33 @@
+package com.xyz;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+    private String name="John";
+
+    private Vehicle vehicle;
+
+    @Autowired
+    public Person(@Qualifier("vehicle3") Vehicle vehicle1) {
+        this.vehicle = vehicle1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+}
